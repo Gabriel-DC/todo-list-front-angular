23418baa9cf1e5c51da783eb9c83c842bd608c12
Gabriel-DC <57327840+Gabriel-DC@users.noreply.github.com>
2022-12-13 19:37:24 -0300

    Adaptando o modal de exclusão
    Fazendo com que o modal seja interno do componente todo-item facilitando o acesso do item.

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class ModalComponent {
   public isOpen: boolean = false;
+  public deleteEvent = new EventEmitter();
 
   toggle() {
     this.isOpen = !this.isOpen;
