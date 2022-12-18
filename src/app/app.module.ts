477c7771438da74731517d1e3dc4bf645a9f54d2
Gabriel-DC <gabrieldcgh@gmail.com>
2022-12-17 22:22:55 -0300

    Aplicação de animação no modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -35,6 +36,7 @@ import { NewTodoButtonComponent } from './components/new-todo-button/new-todo-bu
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,9 +1,26 @@
 import { Component, EventEmitter } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css'],
+  animations: [
+    trigger('overlay', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('250ms', style({ opacity: 0.5 })),
+      ]),
+      transition(':leave', [animate('500ms', style({ opacity: 0 }))]),
+    ]),
+    trigger('modal', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('500ms', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [animate('250ms', style({ opacity: 0 }))]),
+    ]),
+  ],
 })
 export class ModalComponent {
   public isOpen: boolean = false;
