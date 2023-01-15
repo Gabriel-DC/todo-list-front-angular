7057a647fda51afc4a01c1ed3a8394310c1a2933
Gabriel-DC <57327840+Gabriel-DC@users.noreply.github.com>
2023-01-14 21:25:03 -0300

    Removing empty CSS file

--- src/app/pages/today/today.component.ts
+++ src/app/pages/today/today.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-today',
   templateUrl: './today.component.html',
-  styleUrls: ['./today.component.css'],
+  styleUrls: [],
 })
 export class TodayComponent {
   constructor() {}
