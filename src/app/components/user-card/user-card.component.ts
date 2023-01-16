e4cca5d339222c079ee7c88939b3e01e206e27a6
Gabriel-DC <57327840+Gabriel-DC@users.noreply.github.com>
2023-01-16 19:36:25 -0300

    Ajustes UserCard

--- src/app/components/user-card/user-card.component.ts
+++ src/app/components/user-card/user-card.component.ts
@@ -2,6 +2,8 @@ import { firstValueFrom } from 'rxjs';
 import { AfterViewInit, Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { getIdToken } from 'firebase/auth';
+import { User } from 'src/app/models/user';
+import { TitleStrategy } from '@angular/router';
 
 @Component({
   selector: 'app-user-card',
@@ -15,14 +17,19 @@ export class UserCardComponent implements AfterViewInit {
   }
 
   async getUserInfo() {
-    let user = await firstValueFrom(this.afAuth.user);
-    this.user.name = user?.displayName;
-    this.user.picture = user?.photoURL;
+    const { displayName, photoURL, email } =
+      (await firstValueFrom(this.afAuth.user)) || {};
+    this.user = {
+      name: displayName?.toString(),
+      picture: photoURL?.toString() ?? 'https://via.placeholder.com/150',
+      email: email?.toString(),
+    };
   }
 
-  public user: any = {
+  public user: User = {
     name: '',
     picture: 'https://via.placeholder.com/150',
+    email: '',
   };
 
   logout() {
