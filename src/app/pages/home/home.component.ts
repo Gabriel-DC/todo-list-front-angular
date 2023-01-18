99504aef775dc6ba8c208d642d069abfd14c39f9
Gabriel-DC <57327840+Gabriel-DC@users.noreply.github.com>
2023-01-17 23:00:48 -0300

    Remove useless CSS

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,7 +5,6 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
   constructor(private router: Router, private afAuth: AngularFireAuth) {}
